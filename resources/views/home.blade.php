ed001378022b5a7419c91838ca7bf5719ff0df5f
Pedro Vidal <pedrorvidal@gmail.com>
2025-04-12 22:33:25 -0300

    Added project assets

--- resources/views/home.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.main_layout')
-
-@section('content')
-    <h3>Este texto faz parte da view</h3>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    echo "Home";
+    echo "Blade Layouts";
 });
 
 Route::get('/home', function () {
     return view('home');
-});
-
-Route::get('/other', function () {
-    return view('other');
 });
