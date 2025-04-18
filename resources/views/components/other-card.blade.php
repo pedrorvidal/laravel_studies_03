d35248391d260fffe287ae28fcfb0dd60211dac8
Pedro Vidal <pedrorvidal@gmail.com>
2025-04-17 23:59:36 -0300

    slots

---
+++ resources/views/components/other-card.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <h3>Este é o texto inicial do componente</h3>
+
+    {{--vamos exibir o conteúdo do slot aqui comm a diretiva do blade--}}
+    {{ $slot }}
+
+    <h3>texto final do componente</h3>
+</div>
