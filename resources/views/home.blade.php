ccfba6f04b2521bece661430877468c7a11939f5
Pedro Vidal <pedrorvidal@gmail.com>
2025-04-18 22:44:09 -0300

    slots components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.main_layout')
 @section('content')
-    <div class="text-center">
-        @foreach ($pessoas_linguas as $pessoa => $linguas)
-            {{--component--}}
-            <x-card-pessoa :$pessoa :$linguas />
-
-        @endforeach
+    {{--<div class="text-center">--}}
+    {{--    @foreach ($pessoas_linguas as $pessoa => $linguas)--}}
+    {{--        {{--component--}}
+    {{--        <x-card-pessoa :$pessoa :$linguas />--}}
+    {{----}}
+    {{--    @endforeach--}}
+    {{--</div>--}}
+    {{--componentes slots--}}
+    <div class="row">
+        <div class="col">
+            <h4>Como funciona um slot</h4>
+            <x-other-card>
+                <h1 class="text-danger">
+                    Este é o slot
+                </h1>
+            </x-other-card>
+        </div>
     </div>
 @endsection
