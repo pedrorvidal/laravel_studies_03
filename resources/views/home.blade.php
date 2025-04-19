420c5e5a52f7453b10db73f822d4f2cd43fbf5bc
Pedro Vidal <pedrorvidal@gmail.com>
2025-04-18 23:06:10 -0300

    multi slots components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,14 +8,25 @@
     {{--    @endforeach--}}
     {{--</div>--}}
     {{--componentes slots--}}
-    <div class="row">
-        <div class="col">
-            <h4>Como funciona um slot</h4>
-            <x-other-card>
-                <h1 class="text-danger">
-                    Este é o slot
-                </h1>
-            </x-other-card>
-        </div>
-    </div>
+    {{--<div class="row">--}}
+    {{--    <div class="col">--}}
+    {{--        <h4>Como funciona um slot</h4>--}}
+    {{--        <x-other-card>--}}
+    {{--            <h1 class="text-danger">--}}
+    {{--                Este é o slot--}}
+    {{--            </h1>--}}
+    {{--        </x-other-card>--}}
+    {{--    </div>--}}
+    {{--</div>--}}
+    <x-multi-slot>
+        <x-slot:title>
+            Este é o título
+        </x-slot>
+        <x-slot:content>
+            Este é o content
+        </x-slot>
+        <x-slot:footer>
+            Este é o footer
+        </x-slot>
+    </x-multi-slot>
 @endsection
